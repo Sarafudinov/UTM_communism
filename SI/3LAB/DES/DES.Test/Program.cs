647dbbe5b693c05c79e1f353b1a011c95dd77fa2
Nicolae <kolia.sh12345@gmail.com>
2022-11-17 00:51:23 +0200

    DES LAB RD

---
+++ SI/3LAB/DES/DES.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DES.Test {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Input encryption key: ");
+            var key = Console.ReadLine();
+
+            Console.Write("Input message to encrypt: ");
+            var message = Console.ReadLine();
+
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            var encryptedMessage = DES.Encrypt(message, key);
+            Console.WriteLine($"\nEncrypted message: {Convert.ToHexString(encryptedMessage)}");
+
+            var decryptedMessage = DES.Decrypt(encryptedMessage, key);
+            Console.WriteLine($"Decrypted message: {decryptedMessage}\n\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
